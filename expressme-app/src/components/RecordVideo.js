2afd029cbe42cee8c635ab788f5dcfffd8dbc968
Dasa Ponnappan <dasa@manizh.com>
2022-07-30 12:49:06 -0400

    Checking in camera state

--- expressme-app/src/components/RecordVideo.js
+++ expressme-app/src/components/RecordVideo.js
@@ -3,7 +3,7 @@ import UploadVideoToS3 from './UploadVideoToS3';
 
 function RecordVideo({ onSuccessUpload }, { onErrorUpload }) {
     return (
-        <VideoRecorder isFlipped={true}
+        <VideoRecorder isFlipped={false}
             onRecordingComplete={(videoBlob) => {
                 console.log(videoBlob);
                 UploadVideoToS3(videoBlob, onSuccessUpload = { onSuccessUpload }, onErrorUpload = { onErrorUpload });
