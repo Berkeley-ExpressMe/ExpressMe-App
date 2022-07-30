dab582f0b3ca9c16d45657cf7a068c0f0e3a05f9
Dasa Ponnappan <dasa@manizh.com>
2022-07-30 12:54:36 -0400

    Checking in camera controls

--- expressme-app/src/components/RecordVideo.js
+++ expressme-app/src/components/RecordVideo.js
@@ -3,7 +3,7 @@ import UploadVideoToS3 from './UploadVideoToS3';
 
 function RecordVideo({ onSuccessUpload }, { onErrorUpload }) {
     return (
-        <VideoRecorder isFlipped={false}
+        <VideoRecorder isFlipped showReplayControls
             onRecordingComplete={(videoBlob) => {
                 console.log(videoBlob);
                 UploadVideoToS3(videoBlob, onSuccessUpload = { onSuccessUpload }, onErrorUpload = { onErrorUpload });
